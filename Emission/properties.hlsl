6cddcaf7e2c9864e28d796d9b7de16d164b2ce8a
mo___ <nxv256@gmail.com>
2026-07-13 16:51:01 +0900

    1.0
    1.0

---
+++ Emission/properties.hlsl
@@ -0,0 +1,11 @@
+SC_uint(_EmissionEnable, 0, [SCInHeader][SCToggle][SCConstValue(1,pixel)], "", "")
+
+SC_color(_EmissionColor, (1,1,1,1), [SCHDR], "Color", "")
+SC_float(_EmissionBoost, 1, [SCRange(0,1)], "Emission Boost", "")
+SC_uint(_EmissionUseBaseCol, 0, [SCToggle], "Use Base Color", "")
+
+SC_Texture2D(_EmissionTex, "black", [SCCache], "Emission Texture", "")
+SC_uint(_EmissionUseTex, 0, [SCToggle], "Use Texture", "")
+
+SC_uint(_EmissionMaskChannel, 3, [SCCache][SCMaskChannel], "__MaskChannel", "")
+SC_uint(_EmissionUseMask, 1, [SCToggle], "Use Mask", "")
